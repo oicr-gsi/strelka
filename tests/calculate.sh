6d9af52c4a6023233ff59a49ed16f7b5b1b0bd0a
pruzanov <pruzanov@oicr.on.ca>
2021-11-11 10:11:36 -0500

    Modified calculate script

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,5 +5,5 @@ set -o pipefail
 
 cd $1
 
-echo ".vcf files"
-find *vcf* -xtype f -size +0 | xargs md5sum | sort -V
+echo ".vcf files:"
+find *vcf* -xtype f -size +0 | grep -v tbi | xargs md5sum | sort -V
